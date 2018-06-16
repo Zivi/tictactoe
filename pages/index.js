968c3401abbc564e8f0b45d68b28a82868998063
Zivi Weinstock <zivi.weinstock@gmail.com>
2018-06-15 21:56:21 -0700

    Initial project set up

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+
+export default () => (
+  <div>
+    <Head>
+      <title>Tic Tac Toe</title>
+      {/* todo: add favicon */}
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+  </div>
+);
---
+++ readme.md
@@ -0,0 +1,6 @@
+#Tic Tac Toe
+To run application
+
+1.  At the project root level, run `npm install`
+2.  Run the command `npm start`
+3.  Open a browser and navigate to `http://localhost:3000`
